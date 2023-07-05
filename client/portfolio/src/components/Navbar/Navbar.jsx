5933c8eaf4138bb12c34d4f53f9f76d37826d575
Laurentiu Popa <popa.laur.01@gmail.com>
2023-07-05 12:47:40 +0300

    Added dark-light mode

--- client/portfolio/src/components/Navbar/Navbar.jsx
+++ client/portfolio/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable no-unused-vars */
 import LightLogo from '../../assets/logo-white.png'
 import DarkLogo from '../../assets/logo-dark.png'
+import LightMode from '../../assets/light-mode.png'
+import DarkMode from '../../assets/dark-mode.png'
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function Navbar() {
     const navigate = useNavigate()
+    const [dark, setDark] = useState(false)
+
+    useEffect(()=>{
+        document.querySelector("body").setAttribute("data-theme", !dark ? "light" : "dark")
+    },[dark])
+
   return (
     <nav>
-        <img src={DarkLogo} onClick={()=> navigate('/')}/>
+        <img src={!dark ? DarkLogo : LightLogo} onClick={()=> navigate('/')}/>
+        <img src={!dark ? DarkMode : LightMode} onClick={()=>setDark(!dark)}/>
     </nav>
   )
 }
