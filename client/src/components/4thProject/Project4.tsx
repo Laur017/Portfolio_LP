3d5437bff65daabd8f7f5bf8e1e1e26a916c061c
Laurentiu Popa <popa.laur.01@gmail.com>
2023-07-10 12:58:01 +0300

    Update Project4.tsx

--- client/src/components/4thProject/Project4.tsx
+++ client/src/components/4thProject/Project4.tsx
@@ -5,7 +5,33 @@ export default function Project4() {
     const [insDay, setInsDay] = useState<string>("")
     const [insMonth, setInsMonth] = useState<string>("")
     const [insYear, setInsYear] = useState<string>("")
-  return (
+    const [finalDay, setFinalDay] = useState<number>()
+    const [finalMonth, setFinalMonth] = useState<number>()
+    const [finalYear, setFinalYear] = useState<number>()
+    const [month, date, year] = new Date().toLocaleDateString("en-US").split("/")
+
+    console.log(date, month, year)
+    console.log(parseInt(year) - 2001, parseInt(month) - 8, parseInt(date) - 17  )
+  
+    const handleResult = () =>{
+        if(parseInt(month)<parseInt(insMonth)){
+            setFinalYear(parseInt(year)-parseInt(insYear) - 1)
+        } else {
+            setFinalYear(parseInt(year)-parseInt(insYear))
+        }
+        if(parseInt(date)<parseInt(insDay)){
+            setFinalDay(parseInt(date))
+        } else {
+            setFinalDay(parseInt(date) - parseInt(insDay))
+        }
+        if(parseInt(month)<parseInt(insMonth)){
+            setFinalMonth(parseInt(month))
+        } else {
+            setFinalMonth(parseInt(month) - parseInt(insMonth))
+        }
+    }
+
+   return (
     <div className='pr4-main-div'>
         <div>
             <label>
@@ -20,13 +46,13 @@ export default function Project4() {
                 <h5>YEAR</h5>
                 <input type='text' placeholder='YYYY' value={insYear} onChange={(e)=>setInsYear(e.target.value)}/>
             </label>
-            <img src={Arrow} />
+            <img src={Arrow} onClick={handleResult}/>
         </div>
 
         <div>
-            <h1><span>{!insYear ? "--":insYear}</span> years</h1>
-            <h1><span>{!insMonth ? "--":insMonth}</span> months</h1>
-            <h1><span>{!insDay ? "--":insDay}</span> days</h1>
+            <h1><span>{!finalYear ? "--":finalYear}</span> years</h1>
+            <h1><span>{!finalMonth ? "--":finalMonth}</span> months</h1>
+            <h1><span>{!finalDay ? "--":finalDay}</span> days</h1>
         </div>
     </div>
   )
